f4da1d90ef3847cdcceaf610569f2e7e253465ec
Andy Martinez <martin3za10@gmail.com>
2025-07-29 21:03:32 -0400

    initial index base

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import BuildingDashboard from './dashboard';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BuildingDashboard />
+  </React.StrictMode>
+); 
